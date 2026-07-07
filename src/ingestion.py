ac8e69a7ea1c92a53cf0cdf88889f1b2dbde738f
nmango1711 <janicijevic.stefi@gmail.com>
2026-07-08 00:26:44 +0200

    add cleaning & aggregations

--- src/ingestion.py
+++ src/ingestion.py
@@ -2,30 +2,43 @@ import pandas as pd
 from pathlib import Path
 from transformation import transform_data
 
-input_file = Path("./data/raw_data/paid_media_export.csv")
-output_dir = Path("./data/processed_data")
 
-output_dir.mkdir(parents=True, exist_ok=True)
+def process_chunks():
+    
+    print("***********************")
+    print("Starting pipeline")
+    print("***********************")
 
-for i, chunk in enumerate(pd.read_csv(input_file, chunksize = 100000)):
+    input_file = Path("./data/raw_data/paid_media_export.csv")
+    output_dir = Path("./data/processed_chunks")
+    output_dir.mkdir(parents=True, exist_ok=True)
 
-    print(f"Processing chunk {i + 1}")
-    df = transform_data(chunk)
-    # 1. CLEANING
-    # chunk = clean_data(chunk)
+    for i, chunk in enumerate(pd.read_csv(input_file, dtype=str ,chunksize = 100000)):
 
-    # 2. TRANSFORMATION
-    # chunk = transform_data(chunk)
+        print(f"Processing chunk: {i + 1}")
+        df = transform_data(chunk)
 
-    # 3. AGGREGATION INSIDE CHUNK
-    # chunk = aggregate_data(chunk)
+        df = (
+            df.groupby(
+                ["date", "platform", "account_id", "campaign_id"],
+                as_index=False
+            )
+            .agg({
+                "campaign_name": "first",
+                "impressions": "sum",
+                "clicks": "sum",
+                "spend": "sum",
+                "currency": "first",
+                "conversions": "sum",
+                "video_views": "sum"
+            })
+        )
 
-    # 4. WRITE CHUNK RESULT TO DISK
-    print(f"Null dates: {chunk['date'].isna().sum()}")
-    # chunk = chunk.dropna(subset=["date"])
-    output_file = output_dir / f"chunk_{i + 1}.parquet"
-    df.to_parquet(output_file, engine="pyarrow", index=False)
+        output_file = output_dir / f"chunk_{i + 1}.parquet"
+        df.to_parquet(output_file, engine="pyarrow", index=False)
 
-    print(f"Saved {output_file}")
+        print(f"Saved to file: {output_file}")
+        print("-----------------------")
 
-print("All chunks processed")
+process_chunks()
+print("All chunks are processed!!!")
--- src/transformation.py
+++ src/transformation.py
@@ -2,21 +2,135 @@ import pandas as pd
 
 def transform_data(df):
     
-    key_cols = ["date", "platform", "account_id", "campaign_id"]
-    df[key_cols] = df[key_cols].apply(lambda x: x.str.strip())
-    df[key_cols] = df[key_cols].replace(
-        ["", "n/a", "N/A", "null", "NULL",],
-        pd.NA
-    )
-
-    df = df.dropna(subset=key_cols)
-    
-    # Date transformations
+    # "date" transformations
+    df["date"] = df["date"].str.strip()
+    df["date"] = df["date"].replace("", pd.NA)
     df["date"] = pd.to_datetime(df["date"], errors="coerce", format="mixed")
     df = df.dropna(subset=["date"])
+    df = df[df["date"] <= pd.Timestamp.today().normalize()]
     df["date"] = df["date"].dt.strftime("%Y-%m-%d")
 
+    # "platform" transformations
+    df["platform"] = (
+        df["platform"]
+        .str.lower()
+        .str.strip()
+        .str.replace(r"[-_]", " ", regex=True)
+        .str.replace(r"\s+", " ", regex=True)
+    )
+    df["platform"] = df["platform"].replace("", pd.NA)
+    df = df.dropna(subset=["platform"])
+    mapping = {
+        "facebook": "meta",
+        "meta": "meta",
+        "google ads": "google_ads",
+    }
+    df["platform"] = df["platform"].map(mapping)
+    df = df.dropna(subset=["platform"])
+
+    # "account_id" transformations
+    df["account_id"] = (
+        df["account_id"]
+        .str.strip()
+        .str.replace(r"\s+", "", regex=True)
+    )
+    df["account_id"] = df["account_id"].replace("", pd.NA)
+    df = df.dropna(subset=["account_id"])
+
+    # "campaign_id" transformations
+    df["campaign_id"] = (
+        df["campaign_id"]
+        .str.strip()
+        .str.replace(r"\s+", "", regex=True)
+    )
+    df["campaign_id"] = df["campaign_id"].replace("", pd.NA)
+    df = df.dropna(subset=["campaign_id"])
+
+    # "campaign_name" transformations
+    df["campaign_name"] = (
+        df["campaign_name"]
+        .str.lower()
+        .str.strip()
+        .str.replace(r"\s+", " ", regex=True)
+    )
+    df["campaign_name"] = df["campaign_name"].replace("", pd.NA)
+    df["campaign_name"] = df["campaign_name"].fillna("UNKNOWN")
+
+    # "impressions" transformations
+    df["impressions"] = (
+        df["impressions"]
+        .str.replace(",", "", regex=False)
+        .str.strip()
+    )
+    df["impressions"] = df["impressions"].replace("", pd.NA)
+    df["impressions"] = pd.to_numeric(df["impressions"], errors="coerce")
+    df = df.dropna(subset=["impressions"])
+    df = df[df["impressions"] >= 0]
+    df = df[df["impressions"].mod(1) == 0]
+    df["impressions"] = df["impressions"].astype("int64")
+
+    # "clicks" transfomation
+    df["clicks"] = (
+        df["clicks"]
+        .str.replace(",", "", regex=False)
+        .str.strip()
+    )
+
+    df["clicks"] = df["clicks"].replace("", pd.NA)
+    df["clicks"] = pd.to_numeric(df["clicks"], errors="coerce")
+    df = df.dropna(subset=["clicks"])
+    df = df[df["clicks"] >= 0]
+    df = df[df["clicks"].mod(1) == 0]
+    df["clicks"] = df["clicks"].astype("int64")
+
+    # "spend" transfomation
+    df["spend"] = (
+        df["spend"]
+        .str.replace(",", "", regex=False)
+        .str.strip()
+    )
+
+    df["spend"] = df["spend"].replace("", pd.NA)
+    df["spend"] = pd.to_numeric(df["spend"], errors="coerce")
+    df = df.dropna(subset=["spend"])
+    df = df[df["spend"] >= 0]
+    df["spend"] = df["spend"].round(2)
+
+    # "currency" transfomation
+    df["currency"] = (
+        df["currency"]
+        .str.upper()
+        .str.strip()
+    )
     
+    df["currency"] = df["currency"].replace("", pd.NA)
+    currency_map = {
+        "EURO": "EUR",
+    }
+    df["currency"] = df["currency"].replace(currency_map)
+    df["currency"] = df["currency"].fillna("UNKNOWN")
+    
+    # "conversions" transfomation
+    df["conversions"] = (
+        df["conversions"]
+        .str.strip()
+    )
+
+    df["conversions"] = df["conversions"].replace("", pd.NA)
+    df["conversions"] = pd.to_numeric(df["conversions"], errors="coerce")
+    df["conversions"] = df["conversions"].fillna(0)
+    df = df[df["conversions"] >= 0]
+
+    # "video_views" transfomation
+    df["video_views"] = (
+        df["video_views"]
+        .str.strip()
+    )
+
+    df["video_views"] = df["video_views"].replace("", pd.NA)
+    df["video_views"] = pd.to_numeric(df["video_views"], errors="coerce")
+    df["video_views"] = df["video_views"].fillna(0)
+    df = df[df["video_views"] >= 0]
 
     return df
 
